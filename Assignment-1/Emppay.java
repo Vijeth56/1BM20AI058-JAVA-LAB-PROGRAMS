8b0acff7398dec35f77acc6cc21bfe3cdab5e207
Vijeth56 <80960757+Vijeth56@users.noreply.github.com>
2022-12-10 07:14:05 +0530

    Create Emppay.java

---
+++ Assignment-1/Emppay.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import employee.EMP;
+class Emppay
+{
+
+ public static void main(String args[])
+ {
+
+   EMP e = new EMP("ANU","23","Female",12000);
+   e.call();
+   e.display();
+   }
+ }
